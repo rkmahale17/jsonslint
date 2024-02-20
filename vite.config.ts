acd459ae297d234fccf5cfea7ca4c9515c1419b4
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-21 01:02:48 +0530

    update of basur;

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./dist', import.meta.url))
+      '@': fileURLToPath(new URL('/jsonlint/', import.meta.url))
     }
   }
 })
