38e2d13baabd6c9da1980705b76bcb34b6757fae
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-21 01:21:51 +0530

    removing public path from vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/rkmahale17/jsonslint/'
-    : '/',
+
   base: 'https://github.com/rkmahale17/jsonslint/',
   plugins: [
     vue(),
