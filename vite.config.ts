cd47313741e4830d5c791e0a42be0ca675a9c947
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-21 01:10:16 +0530

    update git

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/rkmahale17/jsonslint/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('/jsonlint/', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
