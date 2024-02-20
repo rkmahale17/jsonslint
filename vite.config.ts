941189d40fc82c72d6ab1e9527be940270b359cf
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-21 01:20:08 +0530

    updating raye

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://github.com/rkmahale17/jsonslint/'
+    : '/',
   base: 'https://github.com/rkmahale17/jsonslint/',
   plugins: [
     vue(),
