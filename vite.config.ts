7d0aa3b335429ea5f5f35661c98f91e818d36aa9
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-21 09:45:38 +0530

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   base:
   process.env.NODE_ENV === "production"
-    ? "/jsonslint/" // production
+    ? "/jsonslint/dist" // production
     : "/",
   resolve: {
     alias: {
