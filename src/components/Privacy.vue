62dbdb182da09f8b3f0838c33f91efeaa062fe44
Rahul Mahale <rahulmahale172214@gmail.com>
2024-02-23 14:06:46 +0530

    save

---
+++ src/components/Privacy.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="privacy-policy">
+      <h1>Privacy Policy</h1>
+      <p>At jsonslint.com, the privacy of our visitors is of extreme importance to us. This privacy policy document outlines the types of personal information received and collected by jsonslint.com and how it is used.</p>
+      
+      <h2>1. Collection and Use of Information:</h2>
+      <p>- We do not collect any personal information when you use jsonslint.com to validate, format, or copy-paste JSON data.</p>
+      <p>- The JSON data you input for validation or formatting purposes is processed client-side within your browser and is never transmitted to our servers.</p>
+      
+      <h2>2. Cookies:</h2>
+      <p>- We do not use cookies for tracking or any other purpose on jsonslint.com.</p>
+      <p>- However, please note that third-party services, such as Google AdSense (our advertising partner), may use cookies as part of their services. Please refer to their respective privacy policies for more information.</p>
+      
+      <h2>3. Children's Privacy:</h2>
+      <p>- jsonslint.com is not directed at individuals under the age of 13. We do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us immediately so that we can remove such information.</p>
+      
+      <h2>4. Advertising:</h2>
+      <p>- Our advertising partner is Google AdSense. We use Google AdSense to provide advertisements on jsonslint.com.</p>
+      <p>- Google, as a third-party vendor, uses cookies to serve ads on jsonslint.com.</p>
+      <p>- Users may opt-out of personalized advertising by visiting Google's Ads Settings.</p>
+      
+      <h2>5. Changes to this Privacy Policy:</h2>
+      <p>- We reserve the right to update or change our Privacy Policy at any time. Any changes will be posted here.</p>
+      
+      <p>By using jsonslint.com, you consent to the terms of this Privacy Policy. If you have any questions or concerns regarding this policy, please contact us at [your contact information].</p>
+      
+      <p>This Privacy Policy was last updated on 23,Feb 2024.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PrivacyPolicy',
+  }
+  </script>
+  
+  <style scoped>
+  .privacy-policy {
+    background-color: #000;
+    color: #fff;
+    padding: 20px;
+  }
+  .privacy-policy h1 {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+  .privacy-policy h2 {
+    font-size: 20px;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  .privacy-policy p {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+  </style>
+  
